6f76f302d31e2fc2228bf16f11f2a2caec37d89b
Nashville7 <nashvillejames7@gmail.com>
2019-03-01 10:05:19 +0200

    Done

---
+++ Display data types/js/main.js
@@ -0,0 +1,96 @@
+
+// Array
+// Object
+// String
+// Number
+// Boolean
+// Null
+// Undefined
+// Date
+
+// //use console.log() to display your results in the console.
+
+// ___________________Array____________________
+
+
+let name = ["Nashville","Justin", "Khanyiso"]
+console.log(name);
+
+
+// __________________end of Array______________
+
+
+// _____________________Objects________________
+
+let mobile = {
+    type: "Samsung",
+    model: "S10",
+    Price: "R16 000",
+    color: "Silver"
+}
+
+console.log("Your " + mobile.type +" " + mobile.model + " will cost " + mobile.price + " and it will be " + mobile.color)
+
+// ____________________end of objects______________
+
+
+// _____________________String_____________________
+
+
+let firstname ="Nashville";
+console.log("This is my string");
+console.log(firstname);
+
+// ___________________end of string________________
+
+
+// __________________Numbers_______________________
+
+let x = 45;
+    y = 35;
+
+console.log(x + y);
+
+// _________________end of numbers_________________
+
+
+// ____________________Boolean____________________
+
+
+let a = 45;
+    b = 45;
+    c = 32;
+
+console.log(a == b);
+console.log(a == c);
+
+
+// ______________end of Boolean___________________
+
+
+// ____________________Null_______________________
+
+let justinSalary = null;
+console.log(justinSalary);
+
+// _________________end of Null__________________
+
+
+// ___________________Undefined__________________
+
+let phone;
+
+
+console.log(phone);
+
+// ____________end of undefined_________________
+
+
+// ____________________Date______________________
+
+
+let date = Date();
+
+console.log(date);
+
+// ________________end of date__________________
